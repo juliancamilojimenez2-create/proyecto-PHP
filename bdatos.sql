3d71a488216b2b9fd5f0ecae48086f6795c0fd05
jefersoncarr <jefersoncarranzaruiz@gmail.com>
2025-08-29 19:07:21 -0500

    actualizacion de base dts

---
+++ bdatos.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS tienda2;
+
+USE tienda2;
+
+CREATE TABLE IF NOT EXISTS t_usuarios (
+    id_usuario INT(11) AUTO_INCREMENT NOT NULL,
+    nombre     VARCHAR(100) NOT NULL,
+    apellido   VARCHAR(100) NOT NULL,
+    email      VARCHAR(200) NOT NULL,
+    password   VARCHAR(200) NOT NULL,
+    rol        VARCHAR(20) NOT NULL,
+    imagen     VARCHAR(200) DEFAULT NULL,
+    CONSTRAINT pk_usuario PRIMARY KEY(id_usuario),
+    CONSTRAINT uq_email UNIQUE(email)
+) ENGINE=InnoDB;
+
+INSERT INTO t_usuarios 
+VALUES (NULL, 'Camilo', 'Jimenez', 'camilojimenez16@gmail.com', '123456', 'admin', 'imagen.jpg');
+
+CREATE TABLE IF NOT EXISTS t_categorias(
+    id_categoria        int(11) auto_increment  NOT NULL,
+    nombre_categoria    varchar(100)            NOT NULL,
+    CONSTRAINT pk_categoria     PRIMARY KEY(id_categoria)
+)ENGINE=InnoDb;
